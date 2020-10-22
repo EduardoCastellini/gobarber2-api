9089355d75b70030177beb74419ad4f9076e9482
Eduardo Castellini <eduardo.castellini@tecnospeed.com.br>
2020-10-22 16:43:14 -0300

    cache

--- src/modules/appointments/infra/http/controller/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controller/AppointmentsController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
@@ -8,10 +7,6 @@ export default class AppointmentsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
     const { provider_id, date } = request.body;
-
-    console.log('date: ', date);
-    const parsedDate = parseISO(date);
-    console.log('parsedDate: ', parsedDate);
 
     const createAppointment = container.resolve(CreateAppointmentService);
 
